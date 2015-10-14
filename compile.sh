fb85af22350b4286c6c6648f769be3fd7f782611
cielavenir <cielartisan@gmail.com>
2015-10-14 17:56:39 +0900

    fix sh

--- compile.sh
+++ compile.sh
@@ -5,5 +5,5 @@ gcc -O2 -DSTANDALONE -o 7png applet/7png.c lib/zopfli/*.c lib/popt/*.c lib/zlib/
 #gcc -O2 -DSTANDALONE -o 7ciso applet/7ciso.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/lzmasdk.cpp -lstdc++
 #gcc -O2 -DSTANDALONE -o 7daxcr applet/7daxcr.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/lzmasdk.cpp -lstdc++
 
-gcc -O2 -DSTANDALONE -o 7png applet/zlibrawstdio.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/lzmasdk.cpp -lstdc++
-gcc -O2 -DSTANDALONE -o 7png applet/zlibrawstdio2.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/lzmasdk.cpp -lstdc++
+gcc -O2 -DSTANDALONE -o zlibrawstdio applet/zlibrawstdio.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/lzmasdk.cpp -lstdc++
+gcc -O2 -DSTANDALONE -o zlibrawstdio2 applet/zlibrawstdio2.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/lzmasdk.cpp -lstdc++
