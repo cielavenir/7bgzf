358a5b2cc245a781932c1ef0fd5474bf36577728
cielavenir <cielartisan@gmail.com>
2018-08-12 22:19:06 +0900

    add 7bgzf.so

---
+++ bgzf_compress.c
@@ -0,0 +1,139 @@
+#include "compat.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "lib/zlibutil.h"
+#include "lib/lzma.h"
+
+void write32(void *p, const unsigned int n){
+	unsigned char *x=(unsigned char*)p;
+	x[0]=n&0xff,x[1]=(n>>8)&0xff,x[2]=(n>>16)&0xff,x[3]=(n>>24)&0xff;
+}
+void write16(void *p, const unsigned short n){
+	unsigned char *x=(unsigned char*)p;
+	x[0]=n&0xff,x[1]=(n>>8)&0xff;
+}
+
+__attribute__((constructor)) void init(){
+	lzmaOpen7z();
+}
+__attribute__((destructor)) void finish(){
+	lzmaClose7z();
+}
+
+int bgzf_compress(void *_dst, size_t *_dlen, const void *src, size_t slen, int level){
+abort();
+	if(!slen){
+		if(*_dlen<28)return -1;
+		*_dlen=28;
+		memcpy(_dst,
+			"\x1f\x8b\x08\x04\x00\x00\x00\x00\x00\xff" // header         (10)
+			"\x06\x00BC\x02\x00"                       // extra header   (6)
+			"\x1b\x00"                                 // size(28-1)     (2)
+			"\x03\x00"                                 // null deflation (2)
+			"\x00\x00\x00\x00\x00\x00\x00\x00"         // footer         (8)
+			,28);
+		return 0;
+	}
+
+	int method=DEFLATE_ZLIB;
+	const char* _smethod=getenv("BGZF_METHOD");
+	if(_smethod&&*_smethod){
+		char *smethod=alloca(strlen(_smethod)+1);
+		strcpy(smethod,_smethod);
+		fputs(smethod,stderr);
+		//set level
+		int _level=-1;
+		int l=strlen(smethod);
+		int i=l-1;
+		for(;i>=0 && '0'<=smethod[i]&&smethod[i]<='9';i--){
+			if(_level<0)_level=0;
+			_level*=10;
+			_level+=smethod[i]-'0';
+		}
+		if(_level>=0)level=_level;
+		//set method
+		smethod[i+1]=0;
+		if(!strcasecmp(smethod,"zlib")){
+			method=DEFLATE_ZLIB;
+		}
+		if(!strcasecmp(smethod,"7zip") || !strcasecmp(smethod,"7-zip")){
+			method=DEFLATE_7ZIP;
+		}
+		if(!strcasecmp(smethod,"zopfli")){
+			method=DEFLATE_ZLIB;
+		}
+		if(!strcasecmp(smethod,"miniz")){
+			method=DEFLATE_ZLIB;
+		}
+		if(!strcasecmp(smethod,"slz") || !strcasecmp(smethod,"libslz")){
+			method=DEFLATE_ZLIB;
+		}
+		if(!strcasecmp(smethod,"libdeflate")){
+			method=DEFLATE_ZLIB;
+		}
+	}
+
+	//compress
+	if(*_dlen<26)return -1;
+	size_t dlen=*_dlen-26;
+	unsigned char *dst=((unsigned char*)_dst)+18;
+
+	if(method==DEFLATE_ZLIB){
+		int r=zlib_deflate(dst,&dlen,(const unsigned char*)src,slen,level);
+		if(r){
+			fprintf(stderr,"deflate %d\n",r);
+			return 1;
+		}
+	}
+	if(method==DEFLATE_7ZIP){
+		void *coder=NULL;
+		lzmaCreateCoder(&coder,0x040108,1,level);
+		if(coder){
+			int r=lzmaCodeOneshot(coder,(unsigned char*)src,slen,dst,&dlen);
+			lzmaDestroyCoder(&coder);
+			if(r){
+				fprintf(stderr,"NDeflate::CCoder::Code %d\n",r);
+				return 1;
+			}
+		}else{
+			return -1;
+		}
+	}
+	if(method==DEFLATE_ZOPFLI){
+		int r=zopfli_deflate(dst,&dlen,src,slen,level);
+		if(r){
+			fprintf(stderr,"zopfli_deflate %d\n",r);
+			return 1;
+		}
+	}
+	if(method==DEFLATE_MINIZ){
+		int r=miniz_deflate(dst,&dlen,src,slen,level);
+		if(r){
+			fprintf(stderr,"miniz_deflate %d\n",r);
+			return 1;
+		}
+	}
+	if(method==DEFLATE_SLZ){
+		int r=slz_deflate(dst,&dlen,src,slen,level);
+		if(r){
+			fprintf(stderr,"slz_deflate %d\n",r);
+			return 1;
+		}
+	}
+	if(method==DEFLATE_LIBDEFLATE){
+		int r=libdeflate_deflate(dst,&dlen,src,slen,level);
+		if(r){
+			fprintf(stderr,"libdeflate_deflate %d\n",r);
+			return 1;
+		}
+	}
+	*_dlen=dlen+25;
+	memcpy(_dst,"\x1f\x8b\x08\x04\x00\x00\x00\x00\x00\xff\x06\x00BC\x02\x00",16);
+	write16(_dst+16,*_dlen);
+	write32(dst+dlen,crc32(0,src,slen));
+	write32(dst+dlen+4,slen);
+	*_dlen+=1;
+	return 0;
+}
--- compile.sh
+++ compile.sh
@@ -2,8 +2,8 @@ gcc -O2 -DSTANDALONE -o 7razf applet/7razf.c applet/7razf_testdecode.c lib/zopfl
 gcc -O2 -DSTANDALONE -o 7bgzf applet/7bgzf.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/zlibutil.c lib/miniz.c lib/slz.c lib/libdeflate/libdeflate.c lib/lzmasdk.cpp -lstdc++
 gcc -O2 -DSTANDALONE -o 7gzip applet/7gzip.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/zlibutil.c lib/miniz.c lib/slz.c lib/libdeflate/libdeflate.c lib/lzmasdk.cpp -lstdc++
 gcc -O2 -DSTANDALONE -o 7png applet/7png.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/zlibutil.c lib/miniz.c lib/slz.c lib/libdeflate/libdeflate.c lib/lzmasdk.cpp -lstdc++
-#gcc -O2 -DSTANDALONE -o 7ciso applet/7ciso.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/zlibutil.c lib/miniz.c lib/slz.c lib/libdeflate/libdeflate.c lib/lzmasdk.cpp -lstdc++
-#gcc -O2 -DSTANDALONE -o 7daxcr applet/7daxcr.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/zlibutil.c lib/miniz.c lib/slz.c lib/libdeflate/libdeflate.c lib/lzmasdk.cpp -lstdc++
 
 gcc -O2 -DSTANDALONE -o zlibrawstdio applet/zlibrawstdio.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/zlibutil.c lib/miniz.c lib/slz.c lib/libdeflate/libdeflate.c lib/lzmasdk.cpp -lstdc++
 gcc -O2 -DSTANDALONE -o zlibrawstdio2 applet/zlibrawstdio2.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/zlibutil.c lib/miniz.c lib/slz.c lib/libdeflate/libdeflate.c lib/lzmasdk.cpp -lstdc++
+
+gcc -shared -fPIC -O2 -o 7bgzf.so bgzf_compress.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/memstream.c lib/zlibutil.c lib/miniz.c lib/slz.c lib/libdeflate/libdeflate.c lib/lzmasdk.cpp -lstdc++
