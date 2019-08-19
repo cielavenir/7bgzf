cea4fc5d7f1efcdab4bb2c794d03787f2bd29afb
cielavenir <cielartisan@gmail.com>
2019-08-19 15:40:09 +0900

    update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-# CC="arm-linux-androideabi-gcc -mthumb -march=armv7-a -mfloat-abi=softfp -mfpu=neon"
+# CC=arm-linux-androideabi-gcc CFLAGS="-mthumb -march=armv7-a -mfloat-abi=softfp -mfpu=neon" arch=noarch host_cpu=base_aliases
+# CC=i686-w64-mingw32-gcc CFLAGS="-mmmx -msse -msse2" arch=noarch host_cpu=base_aliases ZLIBNG_X86=1
 
 shopt -s nullglob
 if [ -z "${CC}" ]; then
-	CC="gcc -march=native"
+	CC="gcc"
+	if [ -z "${CFLAGS}" ]; then
+		CFLAGS="-march=native"
+	fi
 fi
 if [ ! -z "${ZLIBNG_X86}" ]; then
 	ZLIBNG_X86="-DX86_CPUID -DX86_QUICK_STRATEGY lib/zlib-ng/arch/x86/*.c"
 fi
+SOURCES="lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/zlib-ng/*.c lib/zlib-ng/arch/arm/*.c ${ZLIBNG_X86} lib/memstream.c lib/zlibutil.c lib/zlibutil_zlibng.c lib/zlibutil_igzip.c lib/miniz.c lib/slz.c lib/libdeflate/deflate_compress.c lib/libdeflate/aligned_malloc.c lib/lzmasdk.c"
 
+mkdir -p bin
+
+make -C lib/isa-l -f Makefile.unx lib
 for i in 7bgzf 7razf 7gzip 7png zlibrawstdio zlibrawstdio2
 do
 	# ZopfliCalculateEntropy uses log, which is implemented in libm.
-	${CC} -O2 -std=gnu99 -DSTANDALONE -o ${i} applet/${i}.c applet/${i}_*.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/zlib-ng/*.c lib/zlib-ng/arch/arm/*.c ${ZLIBNG_X86} lib/memstream.c lib/zlibutil.c lib/zlibutil_zlibng.c lib/miniz.c lib/slz.c lib/libdeflate/deflate_compress.c lib/libdeflate/aligned_malloc.c lib/lzmasdk.c -lm -ldl
+	${CC} -O2 -std=gnu99 ${CFLAGS} -DSTANDALONE -o bin/${i} applet/${i}.c applet/${i}_*.c ${SOURCES} lib/isa-l/bin/isa-l.a -lm -ldl
 done
+make -C lib/isa-l -f Makefile.unx clean
 
-${CC} -O2 -std=gnu99 -shared -fPIC -o 7bgzf.so bgzf_compress.c lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/zlib-ng/*.c lib/zlib-ng/arch/arm/*.c ${ZLIBNG_X86} lib/memstream.c lib/zlibutil.c lib/zlibutil_zlibng.c lib/miniz.c lib/slz.c lib/libdeflate/deflate_compress.c lib/libdeflate/aligned_malloc.c lib/lzmasdk.c -lm -ldl
+export CFLAGS="${CFLAGS} -fPIC"
+make -C lib/isa-l -f Makefile.unx lib
+${CC} -O2 -std=gnu99 ${CFLAGS} -shared -o bin/7bgzf.so bgzf_compress.c ${SOURCES} lib/isa-l/bin/isa-l.a -lm -ldl
+make -C lib/isa-l -f Makefile.unx clean
