4602c586965d21b77dab72fbbda2a32344a62068
cielavenir <cielartisan@gmail.com>
2019-09-20 13:59:13 +0900

    update for windows

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # CC=arm-linux-androideabi-gcc CFLAGS="-mthumb -march=armv7-a -mfloat-abi=softfp -mfpu=neon" arch=noarch host_cpu=base_aliases
-# CC=i686-w64-mingw32-gcc CFLAGS="-mmmx -msse -msse2" arch=noarch host_cpu=base_aliases ZLIBNG_X86=1
-# CC=x86_64-w64-mingw32-gcc CFLAGS="-march=core2 -mfpmath=sse -mmmx -msse -msse2 -msse3 -mno-ssse3" arch=mingw host_cpu=x86_64 ZLIBNG_X86=1
+# CC=i686-w64-mingw32-gcc LIBS="-lole32 -loleaut32 -pthread" CFLAGS="-mmmx -msse -msse2" arch=noarch host_cpu=base_aliases ZLIBNG_X86=1
+# CC=x86_64-w64-mingw32-gcc LIBS="-lole32 -loleaut32 -pthread" CFLAGS="-march=core2 -mfpmath=sse -mmmx -msse -msse2 -msse3 -mno-ssse3" arch=mingw host_cpu=x86_64 ZLIBNG_X86=1
 
 shopt -s nullglob
 if [ -z "${CC}" ]; then
@@ -17,7 +17,7 @@ fi
 if [ ! -z "${ZLIBNG_X86}" ]; then
 	ZLIBNG_X86="-DX86_CPUID -DX86_QUICK_STRATEGY lib/zlib-ng/arch/x86/*.c"
 fi
-SOURCES="lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/zlib-ng/*.c lib/zlib-ng/arch/arm/*.c ${ZLIBNG_X86} lib/memstream.c lib/zlibutil.c lib/zlibutil_zlibng.c lib/zlibutil_igzip.c lib/miniz.c lib/slz.c lib/libdeflate/deflate_compress.c lib/libdeflate/deflate_decompress.c lib/libdeflate/aligned_malloc.c lib/lzmasdk.c"
+SOURCES="lib/zopfli/*.c lib/popt/*.c lib/zlib/*.c lib/zlib-ng/*.c lib/zlib-ng/arch/arm/*.c ${ZLIBNG_X86} lib/memstream.c lib/zlibutil.c lib/zlibutil_zlibng.c lib/zlibutil_igzip.c lib/miniz.c lib/slz.c lib/libdeflate/deflate_compress.c lib/libdeflate/deflate_decompress.c lib/libdeflate/aligned_malloc.c lib/libdeflate/*/cpu_features.c lib/lzmasdk.c"
 
 mkdir -p bin
 
