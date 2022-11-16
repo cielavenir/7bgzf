c20be5860e6f2eb53677a01203d853a8a2e88437
T.Yamada <cielartisan@gmail.com>
2022-11-16 13:46:55 +0900

    Pass LIBRARY_PATH and PATH in compile_open64

--- compile_open64.sh
+++ compile_open64.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 
-: <<"#EOM"
-export PATH=/opt/open64/bin:$PATH
-sudo cp /usr/lib/x86_64-linux-gnu/crt1.o /usr/lib/crt1.o
-sudo cp /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/crti.o
-sudo cp /usr/lib/x86_64-linux-gnu/crtn.o /usr/lib/crtn.o
-#EOM
+# LIBRARY_PATH=/usr/lib/x86_64-linux-gnu will add crt1/i/n.o path
+# PATH=/opt/open64/bin:$PATH will add open64 from /opt/open64
 
 make clean
 echo "Self (Open64)"
 #ZLIBNG_X86=1
-OPEN64=1 ARCH="-I/usr/include/x86_64-linux-gnu -DOPEN64=1 -mmmx -msse -msse2 -msse3 -pthread -D__builtin_bswap32=byteswap32 -D__builtin_bswap64=byteswap64" LIBS="-pthread -ldl" SUFF=_open64 make
+PATH=/opt/open64/bin:$PATH LIBRARY_PATH=/usr/lib/x86_64-linux-gnu OPEN64=1 ARCH="-I/usr/include/x86_64-linux-gnu -DOPEN64=1 -mmmx -msse -msse2 -msse3 -pthread -D__builtin_bswap32=byteswap32 -D__builtin_bswap64=byteswap64" LIBS="-pthread -ldl" SUFF=_open64 make
 ### now LIBS--nolibopen64rt is invalid.
 make clean
 
